7eff6de180c7653b8c2cbfd8777b82fc0e1fee81
Jesse Keating <jkeating at redhat dot com>
2007-07-24 12:43:08 -0400

    release 0.3.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import glob
 
 setup(name='pungi',
-      version='0.3.8',
+      version='0.3.9',
       description='Distribution compose tool',
       author='Jesse Keating',
       author_email='jkeating@redhat.com',
