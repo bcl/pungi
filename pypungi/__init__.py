55d398b5b46979482cc7d95b664d6a3968d320a5
Jesse Keating <jkeating at redhat dot com>
2007-08-15 22:14:09 -0400

    set a proper format for the console output

--- pypungi/__init__.py
+++ pypungi/__init__.py
@@ -49,6 +49,8 @@ class PungiBase():
                             filename=logfile)
 
         # Create the stdout/err streams and only send INFO+ stuff there
+        formatter = logging.Formatter('%(name)s:%(levelname)s: %(message)s')
         console = logging.StreamHandler()
+        console.setFormatter(formatter)
         console.setLevel(logging.INFO)
         logging.getLogger('').addHandler(console)
