788105f59b7709aa3a3f24f5e8c0d05f8b5ea126
Jesse Keating <jkeating at redhat dot com>
2007-09-12 14:36:28 -0400

    - Create a config class that can make using pungi modules easier. (Mark McLoughlin)

---
+++ pypungi/config.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python -tt
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+import os
+import time
+import yum
+
+from ConfigParser import SafeConfigParser
+
+class Config(SafeConfigParser):
+    def __init__(self):
+        SafeConfigParser.__init__(self)
+
+        self.add_section('default')
+
+        self.set('default', 'osdir', 'os')
+        self.set('default', 'sourcedir', 'source')
+        self.set('default', 'debugdir', 'debug')
+        self.set('default', 'isodir', 'iso')
+        self.set('default', 'relnotefilere',
+                 'eula.txt fedora.css GPL README-BURNING-ISOS-en_US.txt RELEASE-NOTES-en_US.html ^RPM-GPG')
+        self.set('default', 'relnotedirre', 'images stylesheet-images')
+        self.set('default', 'relnotepkgs', 'fedora-release fedora-release-notes')
+        self.set('default', 'product_path', 'Packages')
+        self.set('default', 'cachedir', '/var/cache/pungi')
+        self.set('default', 'arch', yum.rpmUtils.arch.getBaseArch(os.uname()[4]))
+        self.set('default', 'name', 'Fedora')
+        self.set('default', 'iso_basename', 'Fedora')
+        self.set('default', 'version', time.strftime('%Y%m%d', time.localtime()))
+        self.set('default', 'flavor', '')
+        self.set('default', 'destdir', os.getcwd())
+        self.set('default', 'bugurl', 'http://bugzilla.redhat.com')
+        self.set('default', 'discs', '1')
+        self.set('default', 'cdsize', '4608.0')
+
