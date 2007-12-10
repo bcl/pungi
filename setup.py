fa1fc86b8df5867199faa8db0ed27d781f6e5840
Jesse Keating <jkeating at redhat dot com>
2007-12-10 18:29:15 -0500

    Remove extra files from tarball

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import glob
 
 setup(name='pungi',
-      version='1.2.3',
+      version='1.2.4',
       description='Distribution compose tool',
       author='Jesse Keating',
       author_email='jkeating@redhat.com',
