a9249e3c315c4f508fad1668ab74457ca05e1b9a
Jesse Keating <jkeating@redhat.com>
2008-06-12 11:24:54 -0400

    Don't import the files that I've removed.

--- src/bin/pungi.py
+++ src/bin/pungi.py
@@ -13,9 +13,8 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 import os
+import pypungi
 import pypungi.config
-import pypungi.gather
-import pypungi.pungi
 import yum
 import pykickstart.parser
 import pykickstart.version
