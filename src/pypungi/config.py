3cc980b49d0fc91433e13618ddbcc47513e93b9d
Jesse Keating <jkeating@redhat.com>
2008-07-08 17:55:55 -0400

    Add a config item for debuginfo
    We need this as a config object so that we can use it later when
    making repodata.

--- src/pypungi/config.py
+++ src/pypungi/config.py
@@ -42,4 +42,5 @@ class Config(SafeConfigParser):
         self.set('default', 'bugurl', 'http://bugzilla.redhat.com')
         self.set('default', 'discs', '1')
         self.set('default', 'cdsize', '650.0')
+        self.set('default', 'debuginfo', "True")
 
