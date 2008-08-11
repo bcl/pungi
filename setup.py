6fc3d4ff799be5fc6328b780b414a440a24d84a2
Jesse Keating <jkeating@redhat.com>
2008-08-11 11:24:13 -0400

    Prep for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import glob
 
 setup(name='pungi',
-      version='2.0.3',
+      version='2.0.4',
       description='Distribution compose tool',
       author='Jesse Keating',
       author_email='jkeating@redhat.com',
--- src/bin/pungi.py
+++ src/bin/pungi.py
@@ -126,7 +126,7 @@ if __name__ == '__main__':
     today = time.strftime('%Y%m%d', time.localtime())
 
     def get_arguments(config):
-        parser = OptionParser(version="%prog 2.0.3")
+        parser = OptionParser(version="%prog 2.0.4")
 
         def set_config(option, opt_str, value, parser, config):
             config.set('default', option.dest, value)
