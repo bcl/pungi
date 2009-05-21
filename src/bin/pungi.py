5d6fc5c54316ca0fcae9c17348141f9053d6f965
Jesse Keating <jkeating@redhat.com>
2009-05-21 10:34:00 -0700

    Prepare for release

--- src/bin/pungi.py
+++ src/bin/pungi.py
@@ -143,7 +143,7 @@ if __name__ == '__main__':
     today = time.strftime('%Y%m%d', time.localtime())
 
     def get_arguments(config):
-        parser = OptionParser(version="%prog 2.0.15")
+        parser = OptionParser(version="%prog 2.0.16")
 
         def set_config(option, opt_str, value, parser, config):
             config.set('pungi', option.dest, value)
