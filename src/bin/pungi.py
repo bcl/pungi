76c3cd9309670d2922ecd1bfadce57ec5630d203
Dennis Gilmore <dennis@ausil.us>
2011-05-26 10:08:43 -0500

    bump to 2.7

--- src/bin/pungi.py
+++ src/bin/pungi.py
@@ -147,7 +147,7 @@ if __name__ == '__main__':
     today = time.strftime('%Y%m%d', time.localtime())
 
     def get_arguments(config):
-        parser = OptionParser(version="%prog 2.6")
+        parser = OptionParser(version="%prog 2.7")
 
         def set_config(option, opt_str, value, parser, config):
             config.set('pungi', option.dest, value)
